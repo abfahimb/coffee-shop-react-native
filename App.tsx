9fa2bd5524de10ecf377b7f6835bf4f71fc5cec6
abfahimb <codeefahim@gmail.com>
2023-12-30 22:48:25 +0600

    [coffee] husky command change

--- App.tsx
+++ App.tsx
@@ -1,37 +1,20 @@
-import {
-  Image,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import HomeScreen from "./src/screen/Home";
-import OnBoardingScreen from "./src/screen/OnBoardingScreen";
-import { OnboardFlow } from "react-native-onboard";
+import React from 'react';
+
+import { OnboardFlow } from 'react-native-onboard';
 
 export default function App() {
   return (
     <OnboardFlow
       pages={[
         {
-          title: "Welcome to my app",
-          subtitle: "This is page 1",
-          imageUri: "",
-          primaryButtonTitle: "this is fahim",
+          title: 'Welcome to my app',
+          subtitle: 'This is page 1',
+          imageUri: '',
+          primaryButtonTitle: 'this is fahim',
         },
       ]}
-      type={"fullscreen"}
+      type={'fullscreen'}
       backgroundImageUri="https://i.ibb.co/M8qykdm/onboarding.png"
     />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff ",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
